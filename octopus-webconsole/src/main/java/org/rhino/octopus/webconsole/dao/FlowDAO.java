9bab3f64a36a002777b38649cf45907d4504f3f8
ABRUZI <37221597@qq.com>
2016-09-10 16:58:34 +0800

    提交数据源配置

--- octopus-webconsole/src/main/java/org/rhino/octopus/webconsole/dao/FlowDAO.java
+++ octopus-webconsole/src/main/java/org/rhino/octopus/webconsole/dao/FlowDAO.java
@@ -3,12 +3,18 @@ package org.rhino.octopus.webconsole.dao;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.annotation.Resource;
+
 import org.rhino.octopus.webconsole.model.FlowDTO;
 import org.rhino.octopus.webconsole.model.PageInfo;
+import org.springframework.orm.ibatis.SqlMapClientTemplate;
 import org.springframework.stereotype.Repository;
 
 @Repository("flowDAO")
 public class FlowDAO {
+	
+	@Resource(name="sqlMapClientTemplate")
+	private SqlMapClientTemplate sqlMapClientTemplate;
 
 	public List<FlowDTO> queryFlowList(FlowDTO query, PageInfo pageInfo){
 		List<FlowDTO> list =  new ArrayList<FlowDTO>();
@@ -18,10 +24,12 @@ public class FlowDAO {
 		flow.setName("测试");
 		list.add(flow);
 		return list;
+		
+		//return (List<FlowDTO>)this.sqlMapClientTemplate.queryForList("octopus.webconsole.flow.queryFlowList", query, pageInfo.getStart(), pageInfo.getPageNum());
 	}
 	
 	public int queryFlowListSize(FlowDTO query){
-		return 1;
+		return (Integer)this.sqlMapClientTemplate.queryForObject("octopus.webconsole.flow.queryFlowListSize", query);
 	}
 	
 	public FlowDTO queryFlowById(String id){
